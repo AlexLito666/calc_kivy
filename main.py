51515d2abe8ee26ff381a6d846a9cb30bfa83f9f
Aleksey Yrganov <80423637+AlexLito666@users.noreply.github.com>
2022-04-14 22:16:17 +0400

    v2

--- main.py
+++ main.py
@@ -11,29 +11,47 @@ Window.size = (400, 500)
 
 
 class MyApp(App):
-    def build(self):
+
+    def add_number(self, instance):
+        self.formula += str(instance.text)
+
+        self.lbl_value.text = self.formula
         
+
+    def result(self, instance):
+        total = eval(self.formula)
+        
+        self.lbl_value.text = str(total)
+        self.formula = ""
+
+    def clear(self, instance):
+        self.lbl_value.text = ""
+        self.formula = ""
+
+    def build(self): 
+        self.formula = ""
+
         layout_box = BoxLayout(orientation="vertical")
         layout_grid = GridLayout(cols = 4, rows = 5)
-        btn1 = Button(text = "C", background_color = (230, 230, 230, .8), color = (72, 206, 255), font_size =25)
-        btn2 = Button(text = "/", background_color = (230, 230, 230, .8), color = (72, 206, 255), font_size =25)
-        btn3 = Button(text = "X", background_color = (230, 230, 230, .8), color = (72, 206, 255), font_size =25)
-        btn4 = Button(text = "=", background_color = (230, 230, 230, .8), color = (72, 206, 255), font_size =25)
-        btn5 = Button(text = "7", background_color = (250, 250, 250), color = (0,0,0), font_size =25)
-        btn6 = Button(text = "8", background_color = (250, 250, 250), color = (0,0,0), font_size =25)
-        btn7 = Button(text = "9", background_color = (250, 250, 250), color = (0,0,0), font_size =25)
-        btn8 = Button(text = "-", background_color = (230, 230, 230, .8), color = (72, 206, 255), font_size =25)
-        btn9 = Button(text = "4", background_color = (250, 250, 250), color = (0,0,0), font_size =25)
-        btn10 = Button(text = "5", background_color = (250, 250, 250), color = (0,0,0), font_size =25)
-        btn11 = Button(text = "6", background_color = (250, 250, 250), color = (0,0,0), font_size =25)
-        btn12 = Button(text = "+", background_color = (230, 230, 230, .8), color = (72, 206, 255), font_size =25)
-        btn13 = Button(text = "1", background_color = (250, 250, 250), color = (0,0,0), font_size =25)
-        btn14 = Button(text = "2", background_color = (250, 250, 250), color = (0,0,0), font_size =25)
-        btn15 = Button(text = "3", background_color = (250, 250, 250), color = (0,0,0), font_size =25)
-        btn16 = Button(text = "0", background_color = (250, 250, 250), color = (0,0,0), font_size =25)
+        btn1 = Button(text = "C", background_color = (230, 230, 230, .8), color = (72, 206, 255), font_size =25, on_press = self.clear)
+        btn2 = Button(text = "/", background_color = (230, 230, 230, .8), color = (72, 206, 255), font_size =25, on_press = self.add_number)
+        btn3 = Button(text = "*", background_color = (230, 230, 230, .8), color = (72, 206, 255), font_size =25, on_press = self.add_number)
+        btn4 = Button(text = "=", background_color = (230, 230, 230, .8), color = (72, 206, 255), font_size =25, on_press = self.result)
+        btn5 = Button(text = "7", background_color = (250, 250, 250), color = (0,0,0), font_size =25, on_press = self.add_number)
+        btn6 = Button(text = "8", background_color = (250, 250, 250), color = (0,0,0), font_size =25, on_press = self.add_number)
+        btn7 = Button(text = "9", background_color = (250, 250, 250), color = (0,0,0), font_size =25, on_press = self.add_number)
+        btn8 = Button(text = "-", background_color = (230, 230, 230, .8), color = (72, 206, 255), font_size =25, on_press = self.add_number)
+        btn9 = Button(text = "4", background_color = (250, 250, 250), color = (0,0,0), font_size =25, on_press = self.add_number)
+        btn10 = Button(text = "5", background_color = (250, 250, 250), color = (0,0,0), font_size =25, on_press = self.add_number)
+        btn11 = Button(text = "6", background_color = (250, 250, 250), color = (0,0,0), font_size =25, on_press = self.add_number)
+        btn12 = Button(text = "+", background_color = (230, 230, 230, .8), color = (72, 206, 255), font_size =25, on_press = self.add_number)
+        btn13 = Button(text = "1", background_color = (250, 250, 250), color = (0,0,0), font_size =25, on_press = self.add_number)
+        btn14 = Button(text = "2", background_color = (250, 250, 250), color = (0,0,0), font_size =25, on_press = self.add_number)
+        btn15 = Button(text = "3", background_color = (250, 250, 250), color = (0,0,0), font_size =25, on_press = self.add_number)
+        btn16 = Button(text = "0", background_color = (250, 250, 250), color = (0,0,0), font_size =25, on_press = self.add_number)
 
 
-        txt_input = TextInput(multiline=False, size_hint =(1, .15), font_size = 40, base_direction ="rtl")
+        self.lbl_value = Label(text = "", size_hint =(1, .15), font_size = 40, halign = 'right')
 
         layout_grid.add_widget(btn1)
         layout_grid.add_widget(btn2)
@@ -53,7 +71,7 @@ class MyApp(App):
         layout_grid.add_widget( btn16)
 
 
-        layout_box.add_widget(txt_input)
+        layout_box.add_widget(self.lbl_value)
         layout_box.add_widget(layout_grid)
 
         return layout_box
